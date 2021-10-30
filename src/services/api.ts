55b0fcac1abd2e998359f76ba6ed6fcf1bcebbaa
GabriellaFGuerra <gabriella.guerra@etec.gov.sp.br>
2021-10-30 12:09:51 -0300

    Fixed API route

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://task-project-gabriella.herokuapp.com/tasks'
+    baseURL: 'http://task-project-gabriella.herokuapp.com/'
 })
 
 export default api;
